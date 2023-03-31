69f21a7df19c6950de082706601df4dabfd0deb5
minami0601 <minami.love.bb@icloud.com>
2023-03-31 22:49:58 +0900

    material UIの導入

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      {/* ツイッターアイコン */}
+      <TwitterIcon className='sidebar--twitterIcon' />
+
+      {/* sidebarOption */}
+      {/* sidebarOption */}
+      {/* sidebarOption */}
+      {/* sidebarOption */}
+      {/* sidebarOption */}
+      {/* sidebarOption */}
+
+      {/* ツイートオプション */}
+    </div>
+  )
+}
+
+export default Sidebar
+
